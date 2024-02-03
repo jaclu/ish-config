3571fe89803deefd95eb497020ba648ad82e4a14
Jacob Lundqvist <jaclu@users.noreply.github.com>
2024-02-03 17:58:31 +0100

    improved dependency handling, now does sudo when needed

--- set-timezone.sh
+++ set-timezone.sh
@@ -2,17 +2,13 @@
 #
 #  Part of https://github.com/jaclu/ish-config
 #
-#  Copyright (c) 2022: Jacob.Lundqvist@gmail.com
+#  Copyright (c) 2022-2024: Jacob.Lundqvist@gmail.com
 #
 #  License: MIT
 #
-#  Extraction of zone time offests derived from
+#  Extraction of zone time offsets derived from
 #  https://gist.github.com/eduardoaugustojulio/fa83cf85efa39919d6a70ca679e91f28
 #
-
-dialog_app="dialog"  # --erase-on-exit"
-# dialog_app="whiptail"
-
 
 do_clear() {
     #
@@ -22,58 +18,53 @@ do_clear() {
     [[ "$dialog_app" = "whiptail" ]] && clear
 }
 
-
 check_dependencies() {
     #
     #  Ensure needed stuff is installed
     #
     apks=()
 
-    dlg_app="$(echo $dialog_app | cut -d ' ' -f 1)"
+    dlg_app="$(echo "$dialog_app" | cut -d ' ' -f 1)"
 
     if [[ -z "$(command -v "$dlg_app")" ]]; then
-        #
-        #  Since whiptail is part of newt, we need to handle that case
-        #
         dependency="$dlg_app"
-        [[ $dependency = "whiptail" ]] && dependency="newt"
+
+        if [[ $dependency = "whiptail" ]] && [[ -f /etc/alpine-release ]]; then
+            #  In Alpine, whiptail is part of newt, correct the dependency
+            dependency="newt"
+        fi
         apks+=("$dependency")
     fi
 
     [[ ! -d /usr/share/zoneinfo ]] && apks+=(tzdata)
 
-    if (( ${#apks[@]} )); then
+    if ((${#apks[@]})); then
         printf 'Installing %s dependencies: ' "$0"
         printf '%s ' "${apks[@]}"
         printf '\n\n'
-        if [ -f /etc/debian_version ]; then
-            cmd="apt install"
+
+        if [[ -f /etc/debian_version ]]; then
+            #  Prevent debians built in dialog,
+            bash -c "DEBIAN_FRONTEND=noninteractive apt install -y ${apks[*]}"
         else
-            cmd="apk add"
+            #  shellcheck disable=SC2068
+            apk add ${apks[@]}
         fi
-        #  shellcheck disable=SC2068
-        $cmd ${apks[@]}
     fi
 }
 
-
-get_tz_regions_lst ()
-{
-    regions=$(find /usr/share/zoneinfo/. -maxdepth 1 -type d | \
-              cut -d "/" -f6 | sed '/^$/d' | awk '{print $0 ""}' | sort)
+get_tz_regions_lst() {
+    regions=$(find /usr/share/zoneinfo/. -maxdepth 1 -type d |
+        cut -d "/" -f6 | sed '/^$/d' | awk '{print $0 ""}' | sort)
 }
 
-
-get_tz_options_lst ()
-{
-options=$(cd /usr/share/zoneinfo/"$1" && find . | sed "s|^\./||" | \
-              sed "s/^\.//" | sed '/^$/d' | sort)
+get_tz_options_lst() {
+    options=$(cd /usr/share/zoneinfo/"$1" && find . | sed 's|^\./||' |
+        sed 's/^\.//' | sed '/^$/d' | sort)
 }
 
-
-main ()
-{
-    tZone=""  # Needs to be cleared in case Back (to main) was selected
+main() {
+    tZone="" # Needs to be cleared in case Back (to main) was selected
     #
     #  Select Region
     #
@@ -85,16 +76,15 @@ main ()
         [[ "$name" = "right" ]] && continue
 
         region_items+=("$name" "")
-    done <<< "$regions"
-
+    done <<<"$regions"
 
     # tested size 20 0 10
-    region=$($dialog_app                                                \
-        --title "Timezones - region"                                    \
-        --backtitle "Region details takes a few seconds to prepare..."  \
-        --ok-button "Next"                                              \
-        --menu "Select a region, or Etc for direct TZ:" 0 0 0           \
-        "${region_items[@]}"                                            \
+    region=$($dialog_app \
+        --title "Timezones - region" \
+        --backtitle "Region details takes a few seconds to prepare..." \
+        --ok-button "Next" \
+        --menu "Select a region, or Etc for direct TZ:" 0 0 0 \
+        "${region_items[@]}" \
         3>&2 2>&1 1>&3-)
 
     if [[ -z "$region" ]]; then
@@ -109,16 +99,15 @@ main ()
     while read -r name; do
         offset=$(TZ="$region/$name" date +%z | sed "s/00$/:00/g")
         option_items+=("$name" " ($offset)")
-    done <<< "$options"
+    done <<<"$options"
 
-
-    menu_title="Select your timezone in\nregion: ${region}"
-    test "$region" = 'Etc' && menu_title="$menu_title\n\nPlease note POSIX and ISO use oposite signs\nfor numerical time-zone references!\nFocus on the right column in this table"
+    menu_title="Select your timezone in\\nregion: ${region}"
+    test "$region" = 'Etc' && menu_title="$menu_title\\n\\nPlease note POSIX and ISO use oposite signs\\nfor numerical time-zone references!\\nFocus on the right column in this table"
     tz=$($dialog_app \
         --title "Timezones - location" \
         --ok-button "Select" \
         --cancel-button "Back" \
-        --menu "$menu_title"  0 0 0 \
+        --menu "$menu_title" 0 0 0 \
         "${option_items[@]}" \
         3>&2 2>&1 1>&3-)
 
@@ -128,15 +117,50 @@ main ()
         #  selected, the outermost layer of this will use it once exiting.
         #
         main
+        # tZone will already be set, so just return now
+        return
+    fi
+
+    selected_tz="$region/$tz"
+    if [[ "$selected_tz" = "/" ]]; then
+        return
+    fi
+
+    if ! $dialog_app --yesno "Setting Time Zone to:\\n\\n    $selected_tz\\n" 0 0; then
+        #  recurse and try again
+        main
     else
         tZone="$region/$tz"
     fi
 }
 
+#===============================================================
+#
+#   Main
+#
+#===============================================================
 
-if [[ "$(whoami)" != 'root' ]]; then
-    echo "You must use sudo or run as root"
-    exit 1;
+#
+#  Dialog is the original and most feature complete implementation.
+#  Whiptail is a slightly simplified fork made by RedHat, and uses newt
+#  instead of ncurses for screen handling, so might have a more
+#  recognizable styling. For this app, either is fine.
+#
+
+dialog_app="whiptail"
+#  dialog_app="dialog" # --erase-on-exit"
+
+TMPDIR="${TMPDIR:-/tmp}"
+
+# execute again as root
+if [[ "$(whoami)" != "root" ]]; then
+    # using $0 instead of full path makes location not hardcoded
+    if ! sudo "$0" "$@"; then
+        echo
+        echo "ERROR: Failed to sudo $0"
+        echo
+    fi
+    exit 0
 fi
 
 check_dependencies
@@ -147,8 +171,8 @@ if test -n "$tZone"; then
     #
     #  Use the selected time-zone
     #
+    echo "Using time zone: $tZone"
     ln -sf "/usr/share/zoneinfo/$tZone" /etc/localtime
-    $dialog_app --msgbox "Time Zone is now:\n\n $tZone\n" 0 0
 fi
 
-do_clear
+# do_clear
